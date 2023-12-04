5c21fae667f4a9cc526e70300fb6001aa49dee27
paulofranklins <paulofranklinc@gmail.com>
2023-12-04 10:23:07 -0800

    Update: added a wait time on healthcheck in backend-cd.yml

--- backend/src/main/java/com/paulofranklins/PingPongController.java
+++ backend/src/main/java/com/paulofranklins/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @GetMapping()
     public PingPong getPingPong() {
-        return new PingPong("Pong #1");
+        return new PingPong("Pong #1.");
     }
 }
