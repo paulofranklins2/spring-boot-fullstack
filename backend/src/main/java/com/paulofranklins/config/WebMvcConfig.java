b570de0203bbfefae3b4f6934cdac0b5caf4a9fa
paulofranklins <paulofranklinc@gmail.com>
2023-12-09 16:10:38 -0800

    implemented: CORS WebConfigurations

---
+++ backend/src/main/java/com/paulofranklins/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.paulofranklins.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("#{'${cors.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
+
+    @Value("#{'${cors.allowed-methods}'.split(',')}")
+    private List<String> allowedMethods;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var corsRegistration = registry.addMapping("/api/**");
+        allowedOrigins.forEach(corsRegistration::allowedMethods);
+        allowedMethods.forEach(corsRegistration::allowedMethods);
+
+    }
+}
