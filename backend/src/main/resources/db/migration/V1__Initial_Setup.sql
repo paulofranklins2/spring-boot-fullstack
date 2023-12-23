665d99a3de4ced6dab850296343a0d5c578bc391
paulofranklins <paulofranklinc@gmail.com>
2023-12-22 22:45:38 -0800

    removed unused import.

--- backend/src/main/resources/db/migration/V1__Initial_Setup.sql
+++ backend/src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -5,8 +5,5 @@ CREATE TABLE customer
     id    INTEGER PRIMARY KEY DEFAULT nextval('customer_id_seq'),
     name  TEXT NOT NULL,
     email TEXT NOT NULL,
---     email TEXT NOT NULL UNIQUE ,
---     password TEXT NOT NULL,
---     gender TEXT NOT NULL,
     age   INT  NOT NULL
 );
