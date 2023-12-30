43a3d6b4d96538044f2c9ebe82a6a2264f03b78d
paulofranklins <paulofranklinc@gmail.com>
2023-12-29 23:28:47 -0800

    added environment.prod.ts settings and updated angular.json file to add a file replacement.

---
+++ frontend/angular/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    api: {
+        baseUrl: 'https://customer-api.paulofranklins.com',
+        authUrl: '/api/v1/auth/login',
+        customerUrl: '/api/v1/customers'
+    }
+};
